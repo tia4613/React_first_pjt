07ffd187d19cb6b6814323b7bc3ee3106055c70a
tia4613 <duqrl3000@gmail.com>
2024-05-16 22:11:27 +0900

    useEffect & Cleanup function

--- src/App.js
+++ src/App.js
@@ -1,29 +1,24 @@
 import { useState, useEffect } from "react";
 
-function App() {
-  const [counter, setValue] = useState(0);
-  const [keyword, setkeyword] = useState("");
-  const onClick = () => setValue((prev) => prev + 1);
-  const onChange = (event) => setkeyword(event.target.value);
-    console.log("tt");
-  useEffect(() => {
-    console.log("cat")
-  }, []);
-  useEffect(() => {
-    if (keyword !== "" && keyword.length > 5)
-      {console.log("search", keyword);}
-  }, [keyword]); // keyword 가 변화할 때 코드를 실행할거라고 알려줌
+function Hello() {
+  function byeFn() {
+    console.log("bye :(");
+  }
+  function hiFn() {
+    console.log("hi! :)");
+    return byeFn;
+  }
+  useEffect(hiFn, []);
+  return <h1>Hello</h1>;
+}
 
+function App() {
+  const [showing, setshowing] = useState(false);
+  const onClick = () => setshowing((prev) => !prev);
   return (
     <div>
-      <input 
-      value= {keyword}
-      onChange={onChange} 
-      type="text" 
-      placeholder="Search here!"
-      />
-      <h1> {counter} </h1>
-      <button onClick={onClick}>click me</button>
+      {showing ? <Hello /> : null}
+      <button onClick={onClick}>{showing ? "Hide" : "show"} </button>
     </div>
   );
 }
