be607e47292973e1c35c682cd7c02928801dc1ab
tia4613 <duqrl3000@gmail.com>
2024-05-16 17:41:44 +0900

    count & keyword

--- src/App.js
+++ src/App.js
@@ -1,11 +1,29 @@
-import Button from "./Button";
-import styles from "./App.module.css";
+import { useState, useEffect } from "react";
 
 function App() {
+  const [counter, setValue] = useState(0);
+  const [keyword, setkeyword] = useState("");
+  const onClick = () => setValue((prev) => prev + 1);
+  const onChange = (event) => setkeyword(event.target.value);
+    console.log("tt");
+  useEffect(() => {
+    console.log("cat")
+  }, []);
+  useEffect(() => {
+    if (keyword !== "" && keyword.length > 5)
+      {console.log("search", keyword);}
+  }, [keyword]); // keyword 가 변화할 때 코드를 실행할거라고 알려줌
+
   return (
     <div>
-      <h1 className={styles.title}> Welcome ! </h1>
-      <Button text={"tt"} />
+      <input 
+      value= {keyword}
+      onChange={onChange} 
+      type="text" 
+      placeholder="Search here!"
+      />
+      <h1> {counter} </h1>
+      <button onClick={onClick}>click me</button>
     </div>
   );
 }
