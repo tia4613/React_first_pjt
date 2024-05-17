2c7942069524403af478038901675f1993baa8a5
tia4613 <duqrl3000@gmail.com>
2024-05-17 12:19:22 +0900

    coin list

--- src/App.js
+++ src/App.js
@@ -1,36 +1,24 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 function App() {
-  const [toDo, setToDo] = useState("");
-  const [toDos, setToDos] = useState([]);
-  const onChange = (event) => setToDo(event.target.value);
-  const onSubmit = (event) => {
-    event.preventDefault()
-    if (toDo === "") {
-      return;
-    }
-    setToDo(""); // 안에 수정하는 값이 들어갈 수 있고 함수가 들어갈 수도 있음
-    setToDos(currentArray => [toDo, ...currentArray]);
-    // ...currentArray 는 []의 안에 인자를 추가해준다.
-  };
-  console.log(toDos);
+  const [loading, setLoading] = useState(true);
+  const [coins, setCoins] = useState([]);
+  useEffect(() => {
+    fetch("https://api.coinpaprika.com/v1/tickers").then((response) => response.json()).then((json) => setCoins(json));
+    setLoading(false);
+  }, []);
   return (
     <div>
-      <h1>My To Dos({toDos.length})</h1>
-      <form onSubmit={onSubmit}>
-        <input 
-        onChange={onChange}
-        value={toDo}
-        type="text" 
-        placeholder="Write your to do ..." 
-        />
-        <button>Add To Do</button>
-      </form>
-      <hr/>
-      <ul> 
-        {toDos.map((item, index) => (
-          <li key={index}>{item}</li>))}
-      </ul>
+      <h1>The Coins!{loading ? "" : `(${coins.length})`}</h1>
+      {loading ? (<strong>Loading ... </strong>) : (
+      <select>
+        {coins.map((coin) => (
+          <option>
+            {coin.name} ({coin.symbol}) : ${coin.quotes.USD.price} USD
+          </option>
+        ))}
+      </select>
+    )}
     </div>
   );
 }
