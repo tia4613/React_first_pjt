e2700f9570f3a6b3ea48832f752e5579e65099a1
tia4613 <duqrl3000@gmail.com>
2024-05-19 23:12:46 +0900

    movie

--- src/App.js
+++ src/App.js
@@ -1,26 +1,18 @@
-import { useEffect, useState } from "react";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+} from "react-router-dom";
+import Home from "./routes/Home";
+import Detail from "./routes/Detail";
 
 function App() {
-  const [loading, setLoading] = useState(true);
-  const [coins, setCoins] = useState([]);
-  useEffect(() => {
-    fetch("https://api.coinpaprika.com/v1/tickers").then((response) => response.json()).then((json) => setCoins(json));
-    setLoading(false);
-  }, []);
-  return (
-    <div>
-      <h1>The Coins!{loading ? "" : `(${coins.length})`}</h1>
-      {loading ? (<strong>Loading ... </strong>) : (
-      <select>
-        {coins.map((coin) => (
-          <option>
-            {coin.name} ({coin.symbol}) : ${coin.quotes.USD.price} USD
-          </option>
-        ))}
-      </select>
-    )}
-    </div>
-  );
+  return <Router>
+    <Routes>
+      <Route path={process.env.PUBLIC_URL + "/"} element={<Home />}/>
+      <Route path="/movie/:id" element={<Detail />}/>
+    </Routes>
+  </Router>;
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,6 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+// import { BrowserRouter } from 'react-router-dom';
+
+// BrowserRouter basename = {process.env.PUBLIC_URL}
+// App
+// /BrowserRouter
 
 ReactDOM.render(
 
